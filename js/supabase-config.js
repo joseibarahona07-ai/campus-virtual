0553d49274674a134a417b7a37d75369a7a7d011
joseibarahona07-ai <joseibarahona07@gmail.com>
2026-07-15 21:09:39 -0600

    Create supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://xhzihaysqyzuulwaxcpy.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhoemloYXlzcXl6dXVsd2F4Y3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQxNDk4MTMsImV4cCI6MjA5OTcyNTgxM30.C4LJBnNK229CvToV7coK-L7v-PWyDhARf91ZnQeWa1o';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
